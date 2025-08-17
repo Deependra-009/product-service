4e32084b4653c3cb55b01dfc8e6af791a0f46cb6
Deependra Trivedi <72593207+Deependra-009@users.noreply.github.com>
2025-08-17 21:19:20 +0530

    Update ProductServiceApplicationTests.java

--- src/test/java/com/shopping/product_service/ProductServiceApplicationTests.java
+++ src/test/java/com/shopping/product_service/ProductServiceApplicationTests.java
@@ -1,7 +1,9 @@
 package com.shopping.product_service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
